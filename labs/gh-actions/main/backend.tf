1649a3bd268eff7b9d6e2745595d8244cd81b87d
ProDataMan <antoine.victor@prodataman.com>
2025-04-30 18:31:21 -0700

    Fix Merge Code

--- labs/gh-actions/main/backend.tf
+++ labs/gh-actions/main/backend.tf
@@ -1,10 +1,6 @@
 terraform {
   backend "s3" {
-<<<<<<< HEAD
     bucket = "my-terraform-state-bucket-antoine123"
-=======
-    bucket = "my-terraform-state-bucket-antoinevictor"
->>>>>>> ea10af5d79729c5c11ca122e1d21945ab350d955
     key    = "terraform.tfstate"
     region = "us-west-1"
   }
